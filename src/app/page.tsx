07e3fa8af9381acdc082d6ba6daece4f1ba6a3d8
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-20 21:53:56 +0530

    fix: minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="bg-dark min-h-screen overflow-hidden relative">
+    <main className="relative min-h-screen overflow-hidden bg-dark">
       <Hero />
-      </main>
+    </main>
   );
 }
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,14 +3,14 @@ import { Button } from "@/components/ui/button";
 const Hero = () => {
   return (
     <section className="flex h-screen flex-col content-center items-center justify-center gap-4 pt-16">
-      <h1 className="font-secondary text-9xl font-bold text-white ml-12">
+      <h1 className="ml-12 font-secondary text-9xl font-bold text-white">
         {"Hack </> Quest"}
       </h1>
       <h3 className="text-2xl text-slate-50">Hurdle | Hack | Hustle</h3>
       <Button variant="neon" size="xl" className="mt-20">
         Register
-        </Button>
-        <div className="absolute h-96 w-96 bg-secondary opacity-40 -top-8 -right-24  blur-[150px] overflow-hidden rounded-full"></div>
+      </Button>
+      <div className="absolute -right-24 -top-8 h-96 w-96 overflow-hidden rounded-full  bg-secondary opacity-40 blur-[150px]"></div>
     </section>
   );
 };
