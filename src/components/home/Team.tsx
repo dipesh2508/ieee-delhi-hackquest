e8ab1e02d22cd0069f2fc920f60fd9b2aa023982
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-26 23:29:51 +0530

    fix:changed Image to img

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -25,12 +25,12 @@ const Team = () => {
             className=" flex w-[300px] justify-center rounded-3xl bg-none opacity-90"
           >
             <CardContent className="relative transform overflow-hidden rounded py-0 shadow-lg transition duration-300 hover:shadow-2xl lg:hover:-translate-y-2">
-              <Image
+              <img
                 src={item.image}
                 className="rounded-xl h-full"
                 width="300"
                 height="300"
-                alt="hello"
+                alt="team image"
               />
               <div className="ml-5 w-[260px] absolute inset-0 flex flex-col justify-center rounded-xl  hover:border-2 bg-black bg-opacity-75 px-5 text-center opacity-0 transition-opacity duration-300 hover:border-headings hover:opacity-100">
                 <CardTitle>{item.position}</CardTitle>
