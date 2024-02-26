4501761e7c29dc1bab7a0b4f6caaecdd4e2ee6bb
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-26 23:27:09 +0530

    chore:update image links

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -6,132 +6,132 @@ export interface Iteam {
 
 export const team:Iteam[] = [
     {
-        image: "/team/Kailash_Maurya.png ",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Kailash_Maurya.png",
         position: "Mentor",
         Name: "Kailash Maurya "  
       },
     {
-        image: "/team/Ashmit_Sharma.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Ashmit_Sharma.png",
         position: "Mentor",
         Name: "Anshul"  
       },
     {
-        image: "/team/Ashmit_Sharma.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Ashmit_Sharma.png",
         position: "Student Section Representative",
         Name: "Ashmit Sharma"  
       },
     {
-      image: "/team/PRERNA_YADAV.png",
+      image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/PRERNA_YADAV.png",
       position: "Student Section Representative",
       Name: "Prerna Yadav"   
     },
     {
-        image: "/team/Utkarsh_Luthra.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Utkarsh_Luthra.png",
         position: "Student Sub-Section Representative",
         Name: "Utkarsh_Luthra"   
       },
       {
-        image: "/team/RIYA_CHOUDHARY.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/RIYA_CHOUDHARY.png",
         position: "Student Sub-Section Representative",
         Name: "Riya Choudhary"   
       },  
     {
-        image: "/team/Sakshi_Singh.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Sakshi_Singh.png",
         position: "NSM LEAD",
         Name: "Sakshi singh"   
       },
     {
-        image: "/team/Sarli_Sengar.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Sarli_Sengar.png",
         position: "NSM Coordinator",
         Name: "Sarli"   
     },
     {
-        image: "/team/Aashi_Sharma.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Aashi_Sharma.png",
         position: "NSM Coordinator",
         Name: "Aashi Sharma"   
     },
     {
-        image: "/team/Shaurya_Mehra.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Shaurya_Mehra.png",
         position: "NSM Coordinator",
         Name: "Shaurya Mehra"    
     },
     {
-        image: "/team/Husain_Saifee.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Husain_Saifee.png",
         position: "NSM Coordinator",
         Name: "Husain Saifee"   
     },
     {
-        image: "/team/ANSH_ARORA.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/ANSH_ARORA.png",
         position: "Design Lead",
         Name: "Ansh Arora"   
     },
     {
-        image: "/team/AYUSH_KUMAR.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/AYUSH_KUMAR.png",
         position: "Design Coordinator",
         Name: "Ayush Kumar"   
     },
     {
-        image: "/team/Abhishek_jha.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Abhishek_Jha.png",
         position: "Design Coordinator",
         Name: "Abhishek Jha"   
     },
     {
-        image: "/team/Yashika_Jain.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Yashika_Jain.png",
         position: "Design Coordinator",
         Name: "Yashika Jain"   
     },
     {
-        image: "/team/Ishita_Choudhary.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Ishita_Choudhary.png",
         position: "Editorial Lead",
         Name: "Ishita Choudhary"   
     },
     {
-        image: "/team/Sahib_Preet_Singh.png",
-        position: "Editorial Coordinator",
-        Name: "Sahib Preet Singh"   
-    },
-    {
-        image: "/team/Dipesh_Ranjan.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Dipesh_Ranjan.png",
         position: "Editorial Coordinator",
         Name: "Dipesh Ranjan"    
     },
     {
-        image: "/team/SNEHA_SONIWAL.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Sahib_Preet_Singh.png",
+        position: "Editorial Coordinator",
+        Name: "Sahib Preet Singh"   
+    },
+    {
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/SNEHA_SONIWAL.png",
         position: "WIE LEAD",
         Name: "Sneha Soniwal"    
     },
     {
-        image: "/team/Riya_Singh.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Riya_Singh.png",
         position: "WIE Coordinator",
         Name: "Riya Singh"     
     },
     {
-        image: "/team/Anshita_Garg.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Anshita_Garg.png",
         position: "WIE Coordinator",
         Name: "Anshita Garg"    
     },
     {
-        image: "/team/Arushi Gupta.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Arushi%20Gupta.png",
         position: "WIE Coordinator",
         Name: "Arushi Gupta"    
     },
     {
-        image: "/team/Prabhjot_Singh.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Prabhjot_Singh.png",
         position: "TAC Coordinator",
         Name: "Prabhjot singh"    
     },
     {
-        image: "/team/Ayush_Rajpurohit.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Ayush_Rajpurohit.png",
         position: "TAC Coordinator",
         Name: "Ayush Sharma"    
     },
     {
-        image: "/team/Kunal Kumar Sharma.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Kunal%20Kumar%20Sharma.png",
         position: "Videography Committee Lead",
         Name: "Kunal Kumar Sharma"    
     },
     {
-        image: "/team/Agastya_J.png",
+        image: "https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/public/team/Agastya_J.png",
         position: "Webmaster",
         Name: "Agastya Jhingan"    
     },
