14281dc12193d02875e9ee1f3c670cdf0b8131ba
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-03-05 21:34:03 +0530

    fix:changes in timeline

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../ui/button";
 import Image from "next/image";
-import About1 from '@/assets/About1.gif'
+import About1 from "@/assets/About1.gif";
 import About2 from "@/assets/About2.jpg";
 import Link from "next/link";
 import { MotionDiv } from "../animation/motionDiv";
@@ -14,7 +14,7 @@ const About = () => {
   return (
     <section
       id="about"
-      className="mt-12 md:grid content-center items-center gap-8 bg-dark bg-opacity-90 px-6 py-12 md:mt-0 grid-cols-1 md:grid-cols-12 md:gap-16 md:px-36"
+      className="mt-12 grid-cols-1 content-center items-center gap-8 bg-dark bg-opacity-90 px-6 py-12 md:mt-0 md:grid md:grid-cols-12 md:gap-16 md:px-36"
     >
       <div className="col-span-12 mb-16 h-1 w-full rounded-full bg-gradient-to-r from-primary to-secondary"></div>
 
@@ -28,10 +28,14 @@ const About = () => {
         }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true, amount: 0.5 }}
-        className="col-span-12 m-auto w-auto bg-white align-middle grid content-center gap-4 rounded-2xl md:col-span-5 md:h-80"
+        className="col-span-12 m-auto grid w-auto content-center gap-4 rounded-2xl bg-white align-middle md:col-span-5 md:h-80"
       >
         {/* <Image src={About1} alt="about" className="rounded-2xl  " /> */}
-        <img src="https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/src/assets/About1.gif" alt="IEEE Delhi SSN" className="rounded-2xl" />
+        <img
+          src="https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/src/assets/About1.gif"
+          alt="IEEE Delhi SSN"
+          className="rounded-2xl"
+        />
       </MotionDiv>
 
       <MotionDiv
@@ -47,7 +51,7 @@ const About = () => {
         className="col-span-12 m-auto flex flex-col content-center gap-6 py-4 md:col-span-7"
       >
         <h2 className="font-secondary text-3xl font-semibold text-headings md:text-5xl">
-          ABOUT IEEE Delhi Section
+          ABOUT IEEE DELHI SECTION
         </h2>
         <p className="text-justify font-primary text-base font-thin text-white md:text-xl">
           IEEE Delhi Section is one of the 13 Sections in India Council coming
@@ -62,7 +66,6 @@ const About = () => {
             <Link href="https://ieeedelhisac.in/">Know More</Link>
           </Button>
         </div>
-       
       </MotionDiv>
 
       <MotionDiv
@@ -77,15 +80,13 @@ const About = () => {
         viewport={{ once: true, amount: 0.5 }}
         className="col-span-12 m-auto flex flex-col content-center gap-8 py-4 md:col-span-7"
       >
-         <h1 className="font-secondary text-3xl font-semibold text-headings md:text-5xl">
+        <h1 className="font-secondary text-3xl font-semibold text-headings md:text-5xl">
           {"ABOUT HACKQUEST"}
         </h1>
         <p className="text-justify font-primary text-base font-thin text-white md:text-xl">
-          Discover the pulse-pounding excitement of HackQuest, the premier event
-          hosted by IEEE Delhi Section Student Activities Committee. Dive into a world of innovation and
-          collaboration, where challenges ignite creativity and connections
-          thrive. Get ready to unleash your potential and conquer cutting-edge
-          technology challenges!
+          {
+            "Discover the pulse-pounding excitement of HackQuest, the premier event hosted by IEEE Delhi Section Student Activities Committee (SAC). Dive into a world of innovation and collaboration, where challenges ignite creativity and connections thrive. Get ready to unleash your potential and conquer cutting-edge technology challenges!"
+          }
         </p>
         <div>
           <Button size={"lg"} className="font-secondary text-lg">
@@ -106,9 +107,13 @@ const About = () => {
         }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true, amount: 0.5 }}
-        className="col-span-12 m-auto w-auto bg-white align-middle grid content-center gap-4 rounded-2xl md:col-span-5"
+        className="col-span-12 m-auto grid w-auto content-center gap-4 rounded-2xl bg-white align-middle md:col-span-5"
       >
-        <img src="https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/src/assets/About2.jpg" alt="Hackquest" className="rounded-2xl" />
+        <img
+          src="https://raw.githubusercontent.com/dipesh2508/ieee-delhi-hackquest/main/src/assets/About2.jpg"
+          alt="Hackquest"
+          className="rounded-2xl"
+        />
         {/* <Image src={About2} alt="about" className="rounded-2xl" /> */}
       </MotionDiv>
 
